6a7078013470d9c4c6263a6cd19a0b810373f76e
wanchalerm.k <ce.wanchalerm@gmail.com>
2023-06-02 19:26:37 +0700

    Init NUXT project

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="con-center">
+     <h1>About</h1>
+    </div>
+  </template>
+  <style>
+    .con-center {
+       text-align: center;
+       margin-top: 1rem;
+    }
+  </style>
